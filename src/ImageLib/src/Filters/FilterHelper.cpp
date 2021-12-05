5a58c2826f3406bf087065c8c75d6ed8844a56b5
AndreyBuyanov <andreew.12.87@gmail.com>
2021-12-05 16:00:35 +0700

    Refactoring

--- src/ImageLib/src/Filters/FilterHelper.cpp
+++ src/ImageLib/src/Filters/FilterHelper.cpp
@@ -28,37 +28,31 @@ void FilterHelper::Convolution(
     if (BitmapFormat::RGBA == source->Format()) {
         pixelSize = 4;
     }
-    std::uint8_t* dst = source->Data();
-    const std::uint8_t* src1 = tempBitmap->Data();
-    const std::uint8_t* src2 = tempBitmap->Data() + (tempBitmap->Width() * pixelSize);
-    const std::uint8_t* src3 = tempBitmap->Data() + (tempBitmap->Width() * pixelSize) * 2;
+    
+    BitmapView dstView { source };
+    BitmapView srcView { tempBitmap.get() };
+
     for (std::uint32_t y = 0; y < source->Height(); y++) {
         for (std::uint32_t x = 0; x < source->Width(); x++) {
-            for (std::uint32_t plane = 0; plane < pixelSize; plane++) {
-                float c =
-                    *(src1 + pixelSize * 0) * kernel[0][0] +
-                    *(src1 + pixelSize * 1) * kernel[0][1] +
-                    *(src1 + pixelSize * 2) * kernel[0][2] +
-                    *(src2 + pixelSize * 0) * kernel[1][0] +
-                    *(src2 + pixelSize * 1) * kernel[1][1] +
-                    *(src2 + pixelSize * 2) * kernel[1][2] +
-                    *(src3 + pixelSize * 0) * kernel[2][0] +
-                    *(src3 + pixelSize * 1) * kernel[2][1] +
-                    *(src3 + pixelSize * 2) * kernel[2][2];
+            for (std::uint32_t channel = 0; channel < pixelSize; channel++) {
+                float c
+                    = srcView.GetPixel(x    , y    , channel) * kernel[0][0]
+                    + srcView.GetPixel(x + 1, y    , channel) * kernel[0][1]
+                    + srcView.GetPixel(x + 2, y    , channel) * kernel[0][2]
+                    + srcView.GetPixel(x    , y + 1, channel) * kernel[1][0]
+                    + srcView.GetPixel(x + 1, y + 1, channel) * kernel[1][1]
+                    + srcView.GetPixel(x + 2, y + 1, channel) * kernel[1][2]
+                    + srcView.GetPixel(x    , y + 2, channel) * kernel[2][0]
+                    + srcView.GetPixel(x + 1, y + 2, channel) * kernel[2][1]
+                    + srcView.GetPixel(x + 2, y + 2, channel) * kernel[2][2];
                 c *= k;
-                *dst = static_cast<std::uint8_t>(clamp(c, F_MIN, F_MAX));
-                src1++;
-                src2++;
-                src3++;
-                dst++;
+                dstView.SetPixel(x, y, channel,
+                    static_cast<std::uint8_t>(clamp(c, F_MIN, F_MAX)));
             }
         }
         if (handler) {
             handler->UpdateProgress(static_cast<std::int32_t>(static_cast<float>(y) / source->Height() * 100));
         }
-        src1 += 2 * pixelSize;
-        src2 += 2 * pixelSize;
-        src3 += 2 * pixelSize;
     }
 }
 
